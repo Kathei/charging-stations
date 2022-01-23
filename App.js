6c33d423d6dfe638a107a1ea116b0a7df87c73d7
Kathei <heiskanen.katriina@gmail.com>
2022-01-23 21:01:11 +0200

    working app with location permission, .gitignore update

--- App.js
+++ App.js
@@ -1,12 +1,78 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from 'react'
+import { StyleSheet, Text, View, TouchableOpacity, ScrollView, SafeAreaView } from 'react-native';
+import axios from 'axios'
+import * as Location from 'expo-location'
 
 export default function App() {
+
+  const [data, setData] = useState()
+  const [status, setStatus] = useState()
+  const [location, setLocation] = useState()
+  const [errorMsg, setErrorMsg] = useState()
+
+  const getChargingStations = async (lat, long) => {
+    const response = await axios.get(
+      `https://api.openchargemap.io/v3/poi?key=${process.env.API_KEY}&latitude=${lat}&longitude=${long}&maxresults=10`
+    )
+    return response.data
+  }
+
+  const onPress = async id => {
+    try {
+    const response = await axios.post(`https://example.ev.energy/chargingsession`,
+    {
+      user: 1,
+      car_id: 1,
+      charger_id: id
+    })
+    console.log('response status: ', response)
+  }
+  catch (e) {
+    setStatus(e.message)
+  }
+  }
+
+  useEffect(async () => {
+
+    const { latitude, longitude } = location.coords
+    const stations = await getChargingStations(latitude, longitude)
+    setData(stations)
+
+  }, [location])  
+
+  useEffect(() => {
+    (async () => {
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container} >
+      <ScrollView>
+        <View style={styles.contentContainer}>
+          <Text style={styles.title}>Allow us to fetch your location and you'll see 10 nearest charging stations!</Text>
+        {errorMsg && <Text>{errorMsg}</Text>}
+        {status && <Text>Charging status: {status}</Text>}
+        {data ? 
+        data.map(station => (
+          <View style={styles.stationContainer} key={station.ID}>
+          <Text>{station.AddressInfo.AddressLine1}</Text>
+          <Text>{station.AddressInfo.Distance.toFixed(2)} miles away</Text>
+          <TouchableOpacity style={styles.button} onPress={() => onPress(station.ID)}><Text>Start charging</Text></TouchableOpacity>
+          </View>
+        ))
+        : <Text>Fetching data...</Text> }
+        </View>
+       </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -16,5 +82,29 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    width: '100%',
+    padding: 16
   },
+  contentContainer: {
+    padding: 16,
+  },
+  title: {
+    fontSize: 24,
+    paddingBottom: 24
+  },
+
+  stationContainer: {
+    borderColor: '#000',
+    borderWidth: 2,
+    borderRadius: 10,
+    padding: 16,
+    marginBottom: 16
+  },
+  button: {
+    borderWidth: 1,
+    borderColor: '#666',
+    borderRadius: 8,
+    padding: 8,
+    marginVertical: 8,
+  }
 });
